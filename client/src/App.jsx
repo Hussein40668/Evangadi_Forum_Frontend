a03c72e4adff730129c496ee2942dc502072dd88
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-03-07 22:50:19 +0300

    Setting up React Router Dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-function App() {
+import React from 'react'
+import Home from './pages/Home/Home'
+import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
+import { Routes, Route } from 'react-router-dom'
 
-
+const App = () => {
   return (
     <>
-      <h1>hello</h1>
+      <Routes>
+        <Route path = "/" element = {<Home />} />
+        <Route path = "/login" element = {<Login />} />
+        <Route path = "/register" element = {<Register />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
+
