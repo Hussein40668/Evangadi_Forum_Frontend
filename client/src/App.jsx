26e9b959110a939df8119c79573ca62f6fdf525d
Hussein Muhammed <Hussein.Muhammed091203@gmail.com>
2025-03-08 07:44:00 +0300

    Login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Home from './pages/Home/Home'
 import Register from './pages/Register/Register'
 import Login from './pages/Login/Login'
 import { Routes, Route } from 'react-router-dom'
+import axios from './axiosConfig'
+import { useNavigate } from 'react-router-dom'
+
 
 const App = () => {
+  const token = localStorage.item('token');
+const navigate = useNavigate();
+  const checkUser = async () => {
+
+try {
+  await axios.get('/users/checkUser', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+} catch (error) {
+  console.log(error.response);
+  navigate('/login');
+}
+    
+  }
+
+   useEffect(() => {
+     checkUser();
+   }, []);
   return (
     <>
       <Routes>
--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,59 @@
 import React from 'react'
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from "../../axiosConfig";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const emailDOM = useRef();
+  const passwordDOM = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const emailValue = emailDOM.current.value;
+    const passwordValue = passwordDOM.current.value;
+
+    if (!emailValue || !passwordValue) {
+      alert('Please fill all the fields');
+      return;
+    }
+
+    try {
+    const {data} = await axios.post('/users/login', {
+        email: emailValue,
+        password: passwordValue,
+      });
+      alert('User logged in successfully');
+      localStorage.setItem('token', data.token);
+      // navigate('/');
+      console.log(data);
+    } catch (error) {
+      alert(error?.response?.data?.msg);
+      console.log(error.response.data);
+    }
+  };
+
   return (
-    <div>
-      Login
-    </div>
-  )
+    <section>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <span>Email :--</span>
+          <input ref={emailDOM} type="text" placeholder="email" />
+        </div>
+        <br />
+
+        <div>
+          <span>Password :--</span>
+          <input ref={passwordDOM} type="text" placeholder="password" />
+        </div>
+        <br />
+
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default Login
